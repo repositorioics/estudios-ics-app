c32ed2ae11657803971ac385577b6f0d7feeadf0
sjcarballo <48483746+sjcarballo@users.noreply.github.com>
2024-06-20 13:46:13 -0600

    Notificación y Vibración
    Se agrega notificación y vibración a la hora que se captura el código de barra y qr.

--- src/ni/org/ics/estudios/appmovil/utils/zbar/SimpleScannerActivity.java
+++ src/ni/org/ics/estudios/appmovil/utils/zbar/SimpleScannerActivity.java
@@ -1,14 +1,21 @@
 package ni.org.ics.estudios.appmovil.utils.zbar;
 
 import android.app.Activity;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v4.app.NotificationCompat;
 import android.view.ViewGroup;
 import ni.org.ics.estudios.appmovil.R;
 
 public class SimpleScannerActivity extends Activity implements ZBarScannerView.ResultHandler, ZBarConstants {
 
     private ZBarScannerView mScannerView;
+    private final static String default_notification_channel_id = "default" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,15 @@ public class SimpleScannerActivity extends Activity implements ZBarScannerView.R
         if (rawResult != null) {
             ZBarcodeFormat zBarcodeFormat = ZBarcodeFormat.getFormatById(rawResult.getBarcodeFormat().getId());
             if (!zBarcodeFormat.getName().trim().equals("NONE")) {
+
+                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                r.play();
+
+                Vibrator v = (Vibrator) getSystemService(getApplicationContext().VIBRATOR_SERVICE);
+                long[] pattern = {0, 1000, 1000, 0};
+                v.vibrate(pattern, -1);
+
                 Intent dataIntent = new Intent();
                 dataIntent.putExtra(SCAN_RESULT, rawResult.getContents());
                 dataIntent.putExtra(SCAN_RESULT_TYPE, rawResult.getBarcodeFormat().getId());
